a716e7acea991415a6a7ca2b536e36d8072ae7b5
Miguel Cam <miguel.cam.mx@gmail.com>
2020-12-24 10:40:54 -0600

    two factor auth :rocket:

--- app/Providers/FortifyServiceProvider.php
+++ app/Providers/FortifyServiceProvider.php
@@ -49,6 +49,17 @@ class FortifyServiceProvider extends ServiceProvider
             return view('auth.reset-password', ['request' => $request]);
         });
 
+        Fortify::verifyEmailView(function () {
+            return view('auth.verify-email');
+        });
+
+        Fortify::confirmPasswordView(function () {
+            return view('auth.password-confirm');
+        });
+
+        Fortify::twoFactorChallengeView(function () {
+            return view('auth.two-factor-challenge');
+        });
 
     }
 }
